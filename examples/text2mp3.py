215140f3df7a33c7e74a66239d43cc0c9e198b0e
luj <sxsfxx@gmail.com>
2023-07-10 16:50:06 +0800

    1) checking existing mp3 file avoid to repeat.

--- examples/text2mp3.py
+++ examples/text2mp3.py
@@ -8,7 +8,7 @@ import math
 import random
 import datetime
 
-TXT_FILE = "边荒传说.txt"
+TXT_FILE = "庆余年.txt"
 if len(sys.argv) == 2 and sys.argv[1].lower().endswith(".txt"):
     TXT_FILE = sys.argv[1]
 
@@ -26,21 +26,26 @@ async def amain(txt_file) -> None:
         fmt = "{{0}}_{{1:0{0}}}_{{2}}.mp3".format(padding)
         v = 0
         for i in range(0, len(text), slice):
-            n = i//slice
+            n = i//slice + 1
             header = "{0}。第{1}册。".format(file_name, n)
-            communicate = edge_tts.Communicate(header+text[i:i+slice], VOICES[v]["ShortName"])
-            mp3_file = os.path.join(path_name, file_name, fmt.format(file_name, n, VOICES[v]["ShortName"]))
-            await communicate.save(mp3_file)
-            print(f"{n}/{count} {datetime.datetime.now()}")
+            communicate = edge_tts.Communicate(header+text[i:i+slice], VOICES[v%len(VOICES)]["ShortName"])
+            mp3_file = os.path.join(path_name, file_name, fmt.format(file_name, n, VOICES[v%len(VOICES)]["ShortName"]))
+            tmp_file = mp3_file + ".tmp"
+            if not os.path.exists(mp3_file):
+                print(f"{n}/{count} {datetime.datetime.now()} ~ ", end="")
+                await communicate.save(tmp_file)
+                os.rename(tmp_file, mp3_file)
+                print(f"{datetime.datetime.now()}")
+            else:
+                print(f"{n}/{count} {datetime.datetime.now()}")
             v += 1
-            v = v%len(VOICES)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop_policy().get_event_loop()
     try:
         mgr = asyncio.run(edge_tts.VoicesManager.create())
         VOICES = [v for v in mgr.voices if v["Locale"].startswith("zh-CN") or v["Locale"].startswith("zh-TW")]
-        print("available language : ", len(VOICES))
+        print("available languages : ", len(VOICES))
         loop.run_until_complete(amain(TXT_FILE))
     finally:
         loop.close()
