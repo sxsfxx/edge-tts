25b0b3a9095c82bcb1f7a90b7e464f66f572f9d5
luj <sxsfxx@gmail.com>
2023-11-09 13:53:00 +0800

    1) split functionalities.

--- examples/text2mp3.py
+++ examples/text2mp3.py
@@ -7,12 +7,13 @@ import sys
 import math
 import random
 import datetime
+import pprint
 
 TXT_FILE = "搜神记-树下野狐.txt"
 if len(sys.argv) == 2 and sys.argv[1].lower().endswith(".txt"):
     TXT_FILE = sys.argv[1]
 
-async def amain(txt_file) -> None:
+async def text_to_mp3(txt_file, VOICES):
     """Main function"""
     path_name, file_name = os.path.split(txt_file)
     file_name, _ = os.path.splitext(file_name)
@@ -24,27 +25,31 @@ async def amain(txt_file) -> None:
         count = math.ceil(len(text)/slice)
         padding = math.floor(math.log10(count)) + 1
         fmt = "{{0}}_{{1:0{0}}}_{{2}}.mp3".format(padding)
-        v = 0
         for n,pos in enumerate(range(0, len(text), slice), 1):
             header = "{0}。第{1}册。".format(file_name, n)
-            communicate = edge_tts.Communicate(header+text[pos:pos+slice], VOICES[v%len(VOICES)]["ShortName"])
-            mp3_file = os.path.join(path_name, file_name, fmt.format(file_name, n, VOICES[v%len(VOICES)]["ShortName"]))
+            mp3_file = os.path.join(path_name, file_name, fmt.format(file_name, n, VOICES[n%len(VOICES)]["ShortName"]))
             tmp_file = mp3_file + ".tmp"
             if not os.path.exists(mp3_file):
                 print(f"{n}/{count} {datetime.datetime.now()} ~ ", end="")
+                communicate = edge_tts.Communicate(header+text[pos:pos+slice], VOICES[n%len(VOICES)]["ShortName"])
                 await communicate.save(tmp_file)
                 os.rename(tmp_file, mp3_file)
                 print(f"{datetime.datetime.now()}")
             else:
                 print(f"{n}/{count} {datetime.datetime.now()}")
-            v += 1
+
+def list_voices():
+    mgr = asyncio.run(edge_tts.VoicesManager.create())
+    VOICES = [v for v in mgr.voices if v["Locale"].startswith("zh-CN") or v["Locale"].startswith("zh-TW")]
+    pprint.pprint(VOICES)
+    return VOICES
+
+def get_voices():
+    return list_voices()
+
+def main():
+    VOICES = get_voices()
+    asyncio.run(text_to_mp3(TXT_FILE, VOICES))
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop_policy().get_event_loop()
-    try:
-        mgr = asyncio.run(edge_tts.VoicesManager.create())
-        VOICES = [v for v in mgr.voices if v["Locale"].startswith("zh-CN") or v["Locale"].startswith("zh-TW")]
-        print("available languages : ", len(VOICES))
-        loop.run_until_complete(amain(TXT_FILE))
-    finally:
-        loop.close()
+    main()
